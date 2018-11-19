4e95950400b4fb0496537b1838b7bbb0fab054f5
Stefan Reichel <stefan.reichel@akelius.de>
2018-11-19 17:50:30 +0100

    feat: add type definitions

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+declare function createValidateJwt(
+  options: createValidateJwt.AuthOptions
+): Function;
+
+export = createValidateJwt;
+
+declare namespace createValidateJwt {
+  export interface AuthOptions {
+    clientId: string;
+    clientSecret: string;
+    domain: string;
+    algorithms: string;
+    audience?: string;
+  }
+}
